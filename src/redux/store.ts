a0070051bc9c33815c8b387b3d270d0e3d8b4d94
Mueem-Nahid <mueem51@gmail.com>
2023-07-14 23:37:35 +0600

    # Feature - add reducer - add rtk query

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice.ts";
+import productReducer from "./features/products/productSlice.ts";
+import {api} from "@/redux/api/apiSlice.ts";
 
 const store = configureStore({
-  reducer: {
-    cart: cartReducer
-  },
+   reducer: {
+      cart: cartReducer,
+      product: productReducer,
+      [api.reducerPath]: api.reducer, // connecting api layer with store
+   },
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
